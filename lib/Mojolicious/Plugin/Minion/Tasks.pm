98c1e0e424fc9333e42a990f685ce0e7a5bec60e
Adrian Crisan <adrian.crisan@elder-studios.co.uk>
2020-02-21 16:59:17 +0200

    Ready to go on cpan

--- lib/Mojolicious/Plugin/Minion/Tasks.pm
+++ lib/Mojolicious/Plugin/Minion/Tasks.pm
@@ -18,19 +18,13 @@ sub register {
 
     # Make sure "tasks" namespace is defined
     die __PACKAGE__, ": missing 'namespace' in config.\n"
-        unless $config->{ namespace };
+        if (!$config->{ namespace });
 
     my $loader  = Module::Loader->new;
     # Find all the modules in the given namespace
     my @modules = $loader->find_modules($config->{ namespace });
 
     foreach my $module (@modules) {
-        if (!$module->isa('Minion::Task')) {
-            $self->app->warn(sprintf('%s is not a minion task.', $module));
-
-            next;
-        }
-
         # And add the module to the list of tasks
         # e.g.: "MyApp::Tasks::SayHello"
         $app->minion->add_task($module => sub {
@@ -60,10 +54,10 @@ sub handle {
 
         if ($ok == 1) {
             # Mark as finished
-            $job->finish($task->finished_result);
+            $job->finish($task->finish);
         } else {
             # Mark as failed
-            $job->fail($task->error_result);
+            $job->fail($task->error);
         }
     } catch {
         $job->app->log->warn($_);
